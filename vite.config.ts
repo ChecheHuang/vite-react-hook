c2143bd047e3d0a8eec6dfbb964e31099bfb2f4c
Carl <qwerfcxzas1@gmail.com>
2023-05-13 22:39:20 +0800

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -17,31 +17,31 @@ const baseConfig = {
     },
   },
 };
-
-export default defineConfig(({ mode }) => {
-  if (mode === "production") {
-    return {
-      ...baseConfig,
-      build: {
-        outDir: "production",
-      },
-      base: "./",
-    };
-  } else {
-    return {
-      ...baseConfig,
-      server: {
-        port:3000,
-        host:"0.0.0.0",
-        open:false,
-        proxy: {
-          "/api": {
-            target: "http://localhost:3000",
-            changeOrigin: true,
-            rewrite:(path)=>path.replace('/api','api')
-          },
+const actions = {
+  production: {
+    build: {
+      outDir: "production",
+    },
+    base: "./",
+  },
+  development: {
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+      open: false,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace("/api", "api"),
         },
       },
-    };
-  }
+    },
+  },
+  default: baseConfig,
+};
+
+export default defineConfig(({ mode='default' }) => {
+  const extendConfig = actions[mode] ? actions[mode] : actions["development"];
+  return { ...baseConfig, ...extendConfig };
 });
